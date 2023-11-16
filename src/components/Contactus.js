a25819f8012c5bd4250bcf2ddc460b134191dd8a
Mohit Ratan <mohitratan2003@gmail.com>
2023-11-16 23:46:42 +0530

    contact us done

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,8 +1,28 @@
 // ContactUs.jsx
-import React from 'react';
-import styles from './ContactUs.module.css'; // Import the CSS module
-
+import { React, useState } from "react";
+import styles from "./ContactUs.module.css"; // Import the CSS module
+import { apiUrl } from "../config";
 const ContactUs = () => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [message, setmessage] = useState("");
+  const handlesubmit = async () => {
+    let url = `${apiUrl}/contactus/`;
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name:name,
+        email:email,
+        message:message
+      }),
+    });
+    const json = await response.json()
+    console.log(json);
+    alert("submitted")
+  };
   return (
     <div className={styles.contactForm}>
       <div className={styles.formContainer}>
@@ -10,17 +30,23 @@ const ContactUs = () => {
         <form>
           <div className={styles.formGroup}>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name" onChange={e => setname(e.target.value)} />
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="email" onChange={e => setemail(e.target.value)}/>
           </div>
           <div className={styles.formGroup}>
             <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message"></textarea>
+            <textarea id="message" name="message" onChange={e => setmessage(e.target.value)}></textarea>
           </div>
-          <button type="button" className='btn btn-primary text-black'>Submit</button>
+          <button
+            type="button"
+            className="btn btn-primary text-black"
+            onClick={handlesubmit}
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
